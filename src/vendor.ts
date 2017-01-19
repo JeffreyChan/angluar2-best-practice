32c84a91d3b60c77ee39907e9f937accb55d8bda
Jeffrey Chan <cdjboy@163.com>
2017-01-19 11:53:46 +0800

    fix vender.ts style

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,12 +1,13 @@
 ﻿import 'jquery/src/jquery';
 import 'bootstrap/dist/js/bootstrap';
 
-import '../static/css/bootstrap.css';
-import '../static/css/bootstrap-theme.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 //E10 viewport hack for Surface/desktop Windows 8 bug 
 import '../static/css/ie10-viewport-bug-workaround.css';
 
-
-
 import '../favicon.ico';
+
+// Other vendors for example jQuery, Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
